9ccd1f97639631e07b44044fb4e7a3b8d1a51faf
Training_H3B.11.15 <Training_H3B.11.15@accenture.com>
2017-12-29 11:13:16 +0530

    Initial commit to First Project

---
+++ src/main/java/MyMavenProject/sampleApplication/App.java
@@ -0,0 +1,16 @@
+package MyMavenProject.sampleApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println( "###XXXXXXXXXXXXXXXXXXXXXX*********" );
+    	System.out.println( "Hello World!" );
+    	System.out.println( "###XXXXXXXXXXXXXXXXXXXXXX*********" );
+    	System.out.println( "venkata Sir how are you?" );
+    }
+}
